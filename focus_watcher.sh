1141db32af7bca2f08cbf844ebea5fafe39395ce
Nounours Heureux <schtroumps31@gmail.com>
2015-02-20 13:55:13 +0100

    Reset wew argument to -a

--- focus_watcher.sh
+++ focus_watcher.sh
@@ -4,7 +4,7 @@
 # focus a window when it is created
 # depends on: wew focus.sh
 
-wew -m 16 | while IFS=: read ev wid; do
+wew -a | while IFS=: read ev wid; do
     case $ev in
         # XCB_CREATE_NOTIFY
         16) focus.sh $wid ;;
