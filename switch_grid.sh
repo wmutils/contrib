846612823767f2132694c8b20f3aaeacb67daef9
Greduan <me@greduan.com>
2015-02-26 17:56:42 -0600

    Merged z3bra's awesome grid logic

--- switch_grid.sh
+++ switch_grid.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #
 # Copyright (c) 2015 Greduan <me@greduan.com>, licensed under the WTFPL license
+# Credit where credit is due, the grid algorithm was written by z3bra
 #
 # When used puts all the windows in a grid and when you focus one of the windows
 # it puts all the windows back to their original location and focuses the window
@@ -8,47 +9,74 @@
 # depends on: wew focus.sh
 
 TEMP=$(mktemp) && wattr xywhi $(lsw) > $TEMP
-AMOUNT=$(wc -l < $TEMP)
+NB=$(wc -l < $TEMP) # NB as in NumBer
 
 # just safety
-if [ $AMOUNT -eq 1 ]; then
+if [ $NB -eq 1 ]; then
     exit
 fi
 
 # user defined
-#BW=${BW:-4} # exactly the width of your borders
-COLS=${COLS:-4} # how many columns do you want?
-ROWS=${ROWS:-2} # how many rows do you want?
+BW=${BW:-4} # width of your borders
+GAP=${GAP:-20} # gap between windows
 
 # get monitor dimensions
 ROOT=$(lsw -r)
 SW=$(wattr w $ROOT)
 SH=$(wattr h $ROOT)
+# reduce screen useable screen area to improve later expressions
+SW=$(( SW - GAP - 2*BW ))
+SH=$(( SH - GAP - 2*BW ))
 
-# figure out the dimensions of the windows
-W=$(( SW / COLS )) # each window's width
-H=$(( SH / ROWS )) # each window's height
+# calculate the size of the grid using the square root of the number of windows
+ROWS=$(echo "sqrt($NB)" | bc)
+COLS=$ROWS
 
-# and their initial locations
-X=0 # initial position
-Y=0 # initial position
-COUNT=0 # first window
+# FOLLOWING WAS WRITTEN BY Z3BRA, don't give me credit for this awesome logic
+# for each row...
+for r in `seq 1 $ROWS`; do
 
-# loop through the windows and make a grid
-while read line; do
-    ID=$(echo $line | cut -d " " -f 5) # current WID
+    # .. if we're on the last row, display all the remaining windows
+    # eg: if we have 12 windows, the square root would be 3 (truncated).
+    # so the script would draw a 3x3 grid. This would leave 3 windows apart. To
+    # avoid this, we set the number of columns of the last row as
+    #
+    #    12 - 3 * (3-1)
+    # => 12 - 3 * 2
+    # => 12 - 6
+    # == 6
+    # so we will have 6 windows on the last row, instead of 3.
+    # This do not lead to the best looking grid, I know (the best one would be
+    # 3x4), but it's the simplest algo I've found. Don't forget we're playing
+    # with shell scripts here, not matlab..
+    test $r -eq $ROWS && COLS=$(( NB - ROWS * (ROWS-1) ))
 
-    wtp $X $Y $W $H $ID
+    # for each column of each row..
+    for c in `seq 1 $COLS`; do
 
-    COUNT=$((COUNT + 1))
+        # exit if we don't have other windows to display
+        test $(( (r-1)*r + c )) -gt $NB && break
 
-    if [ $COUNT -eq $COLS ]; then
-        X=0
-        Y=$(( (H * ROWS) - H ))
-    else
-        X=$((X + W))
-    fi
-done < $TEMP
+        # heigh of windows (total heigh minus gaps and borders)
+        H=$(( SH/ROWS - GAP - BW ))
+        # same for width
+        W=$(( SW/COLS - GAP - BW ))
+
+        # and the tricky part..
+        # The X offset is the width * the actual column (starting from 0) + the
+        # gaps/borders multiplied by the column number (draw it on a sheet of
+        # paper like me, it will make much more sense!
+        X=$(( W * (c-1) + c*(GAP + BW) ))
+        # same for the Y offset
+        Y=$(( H * (r-1) + r*(GAP + BW) ))
+
+        # finally, teleport the window to the place we just calculated.
+        # the sed trick is used to get the corresponding line number in the file
+        # holding the window infos.
+        wtp $X $Y $W $H $(sed "$(( (r-1)*r + c ))p;d" $TEMP | cut -d\  -f5)
+    done
+done
+# END Z3BRA
 
 # listen to wew for our desired event
 wew | while IFS=: read ev wid; do
