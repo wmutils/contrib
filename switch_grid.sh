e9213ff29354d1ff42a984ec52568807b8a0d78e
Greduan <me@greduan.com>
2015-02-26 15:50:49 -0600

    Now switch_grid.sh uses a dumb grid

--- switch_grid.sh
+++ switch_grid.sh
@@ -7,29 +7,47 @@
 # you switched to.
 # depends on: wew focus.sh
 
+TEMP=$(mktemp) && wattr xywhi $(lsw) > $TEMP
+AMOUNT=$(wc -l < $TEMP)
+
+# just safety
+if [ $AMOUNT -eq 1 ]; then
+    exit
+fi
+
+# user defined
+#BW=${BW:-4} # exactly the width of your borders
+COLS=${COLS:-4} # how many columns do you want?
+ROWS=${ROWS:-2} # how many rows do you want?
+
 # get monitor dimensions
 ROOT=$(lsw -r)
 SW=$(wattr w $ROOT)
 SH=$(wattr h $ROOT)
 
-# couple vars we need
-PFW=$(pfw)
-TEMP=$(mktemp) && wattr xywhi $(lsw) > $TEMP
-BW=${BW:-4} # exactly the width of your borders
+# figure out the dimensions of the windows
+W=$(( SW / COLS )) # each window's width
+H=$(( SH / ROWS )) # each window's height
 
-# figure out the dimensions of the windows and locations
-W=$(( SW / $(wc -l < $TEMP) )) # each window's width
-H=$(( SH - BW * 2 )) # each window's height
+# and their initial locations
 X=0 # initial position
 Y=0 # initial position
+COUNT=0 # first window
 
-# loop through them and make a grid or something similar
-# TODO: make an actual grid instead of just putting all the windows together
-#       horizontally
+# loop through the windows and make a grid
 while read line; do
-    ID=$(echo $line | cut -d " " -f 5)
-    wtp $X $Y $((W - BW * 2)) $H $ID
-    X=$((X + W))
+    ID=$(echo $line | cut -d " " -f 5) # current WID
+
+    wtp $X $Y $W $H $ID
+
+    COUNT=$((COUNT + 1))
+
+    if [ $COUNT -eq $COLS ]; then
+        X=0
+        Y=$(( (H * ROWS) - H ))
+    else
+        X=$((X + W))
+    fi
 done < $TEMP
 
 # listen to wew for our desired event
@@ -47,7 +65,7 @@ wew -m 16 | while IFS=: read ev wid; do
             while read line; do
                 wtp $line
             done < $TEMP
-            focus.sh $PFW
+            focus.sh $(pfw)
             exit
             ;;
     esac
