58f1610beb202a6d854f297df78096a5d6552c28
Greduan <me@greduan.com>
2015-02-25 21:37:45 -0600

    Comments and fixed bug: used $(()) instead of $()
    Thanks dcat for bringing to my attention the bug. :)

--- switch_grid.sh
+++ switch_grid.sh
@@ -7,36 +7,40 @@
 # you switched to.
 # depends on: wew focus.sh
 
+# get monitor dimensions
 ROOT=$(lsw -r)
 SW=$(wattr w $ROOT)
 SH=$(wattr h $ROOT)
+
+# couple vars we need
 PFW=$(pfw)
 TEMP=$(mktemp) && wattr xywhi $(lsw) > $TEMP
 BW=${BW:-4} # exactly the width of your borders
 
 # figure out the dimensions of the windows and locations
-W=$(( SW / $(wc -l < $TEMP) ))
-H=$(( SH - BW * 2 ))
-X=0
-Y=0
+W=$(( SW / $(wc -l < $TEMP) )) # each window's width
+H=$(( SH - BW * 2 )) # each window's height
+X=0 # initial position
+Y=0 # initial position
 
 # loop through them and make a grid or something similar
 # TODO: make an actual grid instead of just putting all the windows together
 #       horizontally
 while read line; do
     ID=$(echo $line | cut -d " " -f 5)
-    wtp $((X)) $((Y)) $((W - BW * 2)) $((H)) $ID
+    wtp $X $Y $((W - BW * 2)) $H $ID
     X=$((X + W))
 done < $TEMP
 
-# loop through wew to hear for when you focus a new window
+# listen to wew for our desired event
+# TODO: currently listens for ENTER_WINDOW, should listen for KEY_PRESS/RELEASE
 wew -m 16 | while IFS=: read ev wid; do
     case $ev in
         22)
             while read line; do
                 wtp $line
             done < $TEMP
-       	    focus.sh $wid
+            focus.sh $wid
             exit
             ;;
         *)
@@ -47,8 +51,8 @@ wew -m 16 | while IFS=: read ev wid; do
             exit
             ;;
     esac
-    # TODO: add a thing so that if the same window is clicked it puts windows
-    #       back and focuses the same window
+    # TODO: if the currently focused window is clicked just put everything back
+    #       together
 done
 
 # cleanup
