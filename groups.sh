617fcbc2953ac783c97a94fd4ffb91b390e5d77a
Greduan <me@greduan.com>
2015-02-28 17:28:37 -0600

    Some groundwork done

--- groups.sh
+++ groups.sh
@@ -3,22 +3,45 @@
 # Copyright (c) 2015 Greduan <me@greduan.com>, licensed under the WTFPL
 
 usage() {
-    echo "usage: $(basename $0) <group> [<wid>]"
+    echo "usage: $(basename $0) [-h] [-s wid group] [-m group] [-u group]"
+    echo "       -h shows this help, you can also not provide args"
+    echo "       -s sets WID's group"
+    echo "       -m maps (shows) group"
+    echo "       -u unmaps (hides) group"
     exit 1
 }
 
 # if no arguments are given
-test -z "$1" && usage
+test $# -eq 0 && usage
 
-FSDIR=${FSDIR:-/tmp/groups}
+# I suggest it's under /tmp or somewhere that gets cleaned up at reboot or gets
+# cleaned up after X stops running
+FSDIR=${FSDIR:-/tmp/groups.sh}
 
 # groups dir doesn't exist
 if [ ! -d $FSDIR ]; then
-    # make dir and put all the windows in a default group
+    # make dir to keep track of our stuff
     mkdir -p $FSDIR
-    echo 'state:yes' >> $FSDIR/group.default
-    lsw >> $FSDIR/group.default
+    # file 'active' has a line for each visible group
+    echo 'default' > $FSDIR/active
+    # group.* is the file that keeps track of which window is in which group
+    lsw > $FSDIR/group.default
 fi
 
-# mapw -m WID
-# mapw -u WID
+# define our functions
+
+# sets wid ($1) given to it to the group given to it ($2)
+set_group() {
+}
+
+# shows all the windows in group ($1) given to it
+map_group() {
+    # mapw -m WID
+}
+
+# hides all the windows in group ($1) given to it
+unmap_group() {
+    # mapw -u WID
+}
+
+# argument logic with getopts or whatever
