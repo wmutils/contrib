831a080cf17b71aca705b99bbecb69c119539c79
Greduan <me@greduan.com>
2015-02-28 20:56:40 -0600

    Setup argument logic, testing time

--- groups.sh
+++ groups.sh
@@ -3,15 +3,18 @@
 # Copyright (c) 2015 Greduan <me@greduan.com>, licensed under the WTFPL
 
 usage() {
-    echo "usage: $(basename $0) [-h] [-s wid group] [-m group] [-u group]"
-    echo "       -h shows this help, you can also not provide args"
-    echo "       -s sets WID's group"
-    echo "       -m maps (shows) group"
-    echo "       -u unmaps (hides) group"
+    cat << EOF
+usage: $(basename $0) [-h] [-s wid group] [-m group] [-u group]"
+       -h shows this help, you can also not provide args"
+       -s sets WID's group"
+       -m maps (shows) group"
+       -u unmaps (hides) group"
+EOF
+
     exit 1
 }
 
-# if no arguments are given
+# test for no arguments
 test $# -eq 0 && usage
 
 # I suggest it's under /tmp or somewhere that gets cleaned up at reboot or gets
@@ -30,17 +33,17 @@ fi
 
 # define our functions
 
-# sets WID ($1) given to it to the group given to it ($2)
+# assings WID ($1) to the group ($2)
 set_group() {
-    # delete $1 (WID) from all the group files
+    # delete WID from all the group files
     # TODO: make POSIX compatible, -i is a GNU-ism
     sed -i "/$1/d" $FSDIR/group.*
 
-    # insert $1 (WID) into new group ($2)
+    # insert WID into new group
     echo $1 >> $FSDIR/group.$2
 }
 
-# shows (maps) all the windows in group ($1) given to it
+# shows (maps) all the windows in group ($1)
 map_group() {
     # safety
     if [ ! -f $FSDIR/group.$1 ]; then
@@ -53,7 +56,7 @@ map_group() {
     done < $FSDIR/group.$1
 }
 
-# hides (unmaps) all the windows in group ($1) given to it
+# hides (unmaps) all the windows in group ($1)
 unmap_group() {
     # safety
     if [ ! -f $FSDIR/group.$1 ]; then
@@ -66,4 +69,21 @@ unmap_group() {
     done < $FSDIR/group.$1
 }
 
-# argument logic with getopts or whatever
+# argument logic
+
+while getopts "hs:m:u:" opt; do
+    case $opt in
+        h)
+            usage
+            ;;
+        s)
+            set_group $OPTARG \$$OPTIND # cool little trick here ;)
+            ;;
+        m)
+            map_group $OPTARG
+            ;;
+        u)
+            unmap_group $OPTARG
+            ;;
+    esac
+done
