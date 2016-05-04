2826a3420a29d7713e75603a78574713079a22b3
Anders Lewis <greagle3@gmail.com>
2016-05-03 17:20:15 -0700

    updated for suggested changes

--- workspace.sh
+++ workspace.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-#
-#copyright agloo in 2016 under the WTFPL 
 #constructs and groups windows into workspaces to switch between using your favorite keybinder.
 
 NUM_WS=9
@@ -9,22 +7,23 @@ help() {
 	cat << EOF
 usage: $(basename $0) [-hinp] [-g ws_num] [-m ws_num] 
 	-h: Displays this message
-	-i: Initializes workspaces and puts you in workspace 0. Should be called once in a startup script.
+	-i: Initialize workspaces. Should be called once in a startup script.
 	-n: Move up one workspace
 	-p: Move down one workspace
-	-g, --ws_num: go to the workspace specified by ws_num
-	-m, --ws_num: move the currently focused window to the worskpace specified by ws_num
+	-g, <ws_num>: go to the workspace specified by <ws_num>
+	-m, <ws_num>: move the currently focused window to the worskpace specified by <ws_num>
 EOF
 	exit 1
 }
 #initializes us in workspace 0
 ws_init() {
-	mkdir /tmp/workspaces/
-	for i in {0..$NUM_WS}
-	do
-		touch /tmp/workspaces/ws"$i"
+	mkdir -p /tmp/workspaces/
+	i = 0
+	while [ "$i" -le "$NUM_WS" ]; do
+		:> /tmp/workspaces/ws"$i"
+		i=$(expr $i + 1)
 	done
-	printf 0 > /tmp/workspaces/curr
+	echo 0 > /tmp/workspaces/curr
 }
 #saves all of the workspaces 
 save_ws() {
@@ -33,18 +32,14 @@ save_ws() {
 }
 move_to_ws() {
 	ws_num=$1;
-	if [ ws_num -gt $NUM_WS ]; then
-		echo "We don't have that workspace"
+	if [ $ws_num -gt $NUM_WS ] || [ $ws_num -lt 0 ]; then
+		echo "Workspace not found"
 		return
 	fi
 	save_ws;
-	for wid in $(lsw); do
-		mapw -u $wid
-	done
-	for wid in $(cat /tmp/workspaces/ws"$ws_num"); do
-		mapw -m $wid
-	done
-	printf $ws_num > /tmp/workspaces/curr
+	mapw -u $(lsw)
+	mapw -m $(cat /tmp/workspaces/ws"$ws_num")
+	echo $ws_num > /tmp/workspaces/curr
 }
 next_ws() {
 	#get what ws we're currently in
@@ -64,9 +59,8 @@ prev_ws() {
 	curr=$(expr $curr - 1);
 
 	#take care of loopback
-	printf $curr
 	if [ $curr -lt 0 ]; then
-		curr=9;
+		curr=$NUM_WS
 	fi
 
 	move_to_ws $curr;
@@ -75,11 +69,11 @@ move_focused_window() {
 	wid=$(pfw);
 	curr_ws=$(cat /tmp/workspaces/curr);
 	if [ $wid != curr_ws ]; then
-		printf $(pfw) >> /tmp/workspaces/ws"$1";
+		pfw >> /tmp/workspaces/ws"$1";
 		mapw -u $wid;
 	fi
 }
-while getopts ":m:g:npih" opt; do
+while getopts ":m:g:npi" opt; do
 	case $opt in
 		n)
 			next_ws;;
@@ -91,10 +85,7 @@ while getopts ":m:g:npih" opt; do
 			move_focused_window $OPTARG;;
 		i)
 			ws_init;;
-		h)
-			help;;
 		\?)
-			help
-			exit 1;;
+			help;;
 	esac
 done
