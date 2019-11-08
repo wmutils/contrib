cf721ae4599a730d0d1a34b60aad13bb3237b3b0
lwilletts <wildefur@gmail.com>
2019-11-08 16:57:24 +0000

    focus_watcher.sh: apply recommended changes

--- focus_watcher.sh
+++ focus_watcher.sh
@@ -4,9 +4,9 @@
 # focus a window when it is created
 # depends on: wew focus.sh
 
-wew | while IFS="$(printf '\t')" read -r ev wid; do
+wew | while read ev wid args; do
     case $ev in
         # occurs on mapping requests
-        "MAP") wattr o "$wid" || focus.sh "$wid" ;;
+        MAP) wattr o $wid || focus.sh $wid ;;
     esac
 done
